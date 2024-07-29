38472ec3f00718609c6e93c3f241d2cceaa5abd9
radhika <radhika.singhal@rea-group.com>
2024-07-30 02:38:16 +0530

    Refactor category images

--- Ecommerce/app/viewModel/ShopScreenViewModel.ts
+++ Ecommerce/app/viewModel/ShopScreenViewModel.ts
@@ -4,10 +4,10 @@ import { Category } from '../models/Category';
 import { Product } from '../models/Product';
 
 const categoryImages: { [key: string]: any } = {
-  "electronics": '@/assets/images/electronic.png',
-  "jewelery": '@/assets/images/jewelery.png',
-  "men's clothing": '@/assets/images/mens-clothing.png',
-  "women's clothing":'@/assets/images/women-clothing.png',
+  "electronics": 'https://s3-alpha-sig.figma.com/img/e350/65d5/55b8804e24889a7e678b8503269047e8?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XHf5zGohthjXsq-hlKoMJMTxh5MHZSS1CU68t4iAnWPRhB1EqaPDFPHzJJCzmAoYjXyLYPEMaXxNcaxLLR4DSkA5f7WbsfMizs95E~3Nf~W2qeUIXJH5csotN2UtnlakQXGf4EQGH8UVTKsqqW85-cjDsPrBBubUS5REjK4gQf5s8ZMrNjz-ca02B2e156lj3N27~H9MBwSP3V5g4qNnU8RbsFxUp1pRD9qa8FFswB1HT4CZKgW4nFQ6eCsMaohZsdEiQvu8b-6ioYa4blECeRubTEWlDI9BXMwf~a9Yw2EtWNN9Vrt9nDEfJdqQV59Aijxt2-Thjr1YK45VTvOoXA__',
+  "jewelery": 'https://s3-alpha-sig.figma.com/img/e350/65d5/55b8804e24889a7e678b8503269047e8?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XHf5zGohthjXsq-hlKoMJMTxh5MHZSS1CU68t4iAnWPRhB1EqaPDFPHzJJCzmAoYjXyLYPEMaXxNcaxLLR4DSkA5f7WbsfMizs95E~3Nf~W2qeUIXJH5csotN2UtnlakQXGf4EQGH8UVTKsqqW85-cjDsPrBBubUS5REjK4gQf5s8ZMrNjz-ca02B2e156lj3N27~H9MBwSP3V5g4qNnU8RbsFxUp1pRD9qa8FFswB1HT4CZKgW4nFQ6eCsMaohZsdEiQvu8b-6ioYa4blECeRubTEWlDI9BXMwf~a9Yw2EtWNN9Vrt9nDEfJdqQV59Aijxt2-Thjr1YK45VTvOoXA__',
+  "men's clothing": 'https://s3-alpha-sig.figma.com/img/8d40/b2ff/9a729542c316774eec94b994b83da2b9?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VXfwuq1-uXX3Tyo5PmWoBqR4SKki8kGKVFU3z269kJutBguuJ8nACGuvoGq~t1pmi73apoXq3yqSrqxP6Vxz4ytGw8MyiZ7enDYwRy3eGZlVoN2rEtwOnJQxZwTsHSZC3EaGiHknIBwgPwJaujw2dBpmTG4R6-gwwk6Z0~fkx1cOW-Bv4ksQmIZHhqjly1S2BsMh0XCSJKXGYRvRXRyDOVsPeVCnOec2g2Se6ndzu67LirECzwgIo~Uhzuy8bgWzyYl74m-A2QMg-2G1K5RukVtPmVNWMJhggnsvJzESRDBLHFxC1ocllThCRBJ5tdm2qDGEwFFeiWm8xHTduGdDsw__',
+  "women's clothing":'https://s3-alpha-sig.figma.com/img/a70b/d3f4/a5802a7e95c2f618b15c7c34d77bb9d1?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ATlTxplN-uTqJlKZvffI0GDq4hXUTSEJ4-2g3mTCbA6emfO65JcYpZ8oN677cDrFCOojSMJ3Cz4DvEJV52ak~MKc3UvSeOwfWn30ZEi~HxaZitRnvt6~IWeqREnc3Wkce9U9U9dmbN~EtdD7QWfUl3AMJBLQQmLhnY6Yonqvy347IHDYHKRVv4cLZJpcZC4Rc2yQAEEyXY665ymDxj9~2A2pHHzCG~Qt0ncNLzuFT7WXxZC~WbIPwgkxCwAQH7ssOB7Kik0xWPYWvF36XZd504wglFNtHVm4wrp3GX7wwPzhM8u3Y5Hsb66HYuY4mVXz~cv6ktMnw8nEqkz5ckfQ-A__',
 };
 
 export const useShopScreenViewModel = (initialCategory = 'All') => {
@@ -28,7 +28,7 @@ export const useShopScreenViewModel = (initialCategory = 'All') => {
         const formattedCategories = data.map((category: string, index: number) => ({
           id: index.toString(),
           name: category,
-          image: categoryImages[category] || 'https://via.placeholder.com/150',
+          image: categoryImages[category],
         }));
         setCategories(formattedCategories);
       } catch (error) {
