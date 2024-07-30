7f58c6b40f0ff9e80c5cfbb109e2757c84228260
radhika <radhika.singhal@rea-group.com>
2024-07-30 16:47:42 +0530

    Reformat code

--- Ecommerce/app/screens/ShopScreen.tsx
+++ Ecommerce/app/screens/ShopScreen.tsx
@@ -1,38 +1,46 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from "react";
 import {
-  View, Text, TouchableOpacity, StyleSheet, SafeAreaView,
-  FlatList, Image, ScrollView, TextInput, Modal
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { useShopScreenViewModel } from '../viewModel/ShopScreenViewModel';
-import { TEXT } from '@/constants/Text';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { useNavigation } from 'expo-router';
-import { RootStackParamList } from '../_layout';
-import { Colors } from '@/constants/Colors';
-import { useFavorites } from '../context/FavoriteContext';
-import { Product } from '../models/Product';
-import { useCart } from '../context/CartContext';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Image,
+  ScrollView,
+  TextInput,
+  Modal,
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { useShopScreenViewModel } from "../viewModel/ShopScreenViewModel";
+import { TEXT } from "@/constants/Text";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { useNavigation } from "expo-router";
+import { RootStackParamList } from "../_layout";
+import { Colors } from "@/constants/Colors";
+import { useFavorites } from "../context/FavoriteContext";
+import { Product } from "../models/Product";
+import { useCart } from "../context/CartContext";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function ShopScreen() {
-  const [searchText, setSearchText] = useState('');
-  const [selectedSort, setSelectedSort] = useState<string>('recommended');
+  const [searchText, setSearchText] = useState("");
+  const [selectedSort, setSelectedSort] = useState<string>("recommended");
   const [isModalVisible, setModalVisible] = useState(false);
-  const [modalType, setModalType] = useState<string>('sort');
-const { favoriteProducts, toggleFavorite } = useFavorites();
-  const { togglecart, cartProducts} = useCart()
+  const [modalType, setModalType] = useState<string>("sort");
+  const { favoriteProducts, toggleFavorite } = useFavorites();
+  const { togglecart, cartProducts } = useCart();
 
-
-
-  type ShopScreenNavigationProp = StackNavigationProp<RootStackParamList, 'ShopScreen'>;
+  type ShopScreenNavigationProp = StackNavigationProp<
+    RootStackParamList,
+    "ShopScreen"
+  >;
   const navigation = useNavigation<ShopScreenNavigationProp>();
 
-  const {
-    categories, products, selectedCategory,
-    setSelectedCategory
-  } = useShopScreenViewModel(selectedCategory);
-
+  const { categories, products, selectedCategory, setSelectedCategory } =
+    useShopScreenViewModel(selectedCategory);
+  
+   
   const filteredProducts = useMemo(() => {
     const lowercasedSearchText = searchText.toLowerCase();
     return products.filter((product) =>
@@ -40,11 +48,10 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
     );
   }, [searchText, products]);
 
-    const sortedProducts = useMemo(() => {
+  const sortedProducts = useMemo(() => {
+    const parsedProducts = filteredProducts.map((product) => {
+      const parsedPrice = parseFloat(product.price.replace(/[^0-9.-]+/g, ""));
 
-    const parsedProducts = filteredProducts.map(product => {
-    const parsedPrice = parseFloat(product.price.replace(/[^0-9.-]+/g, ''));
-    
       return {
         ...product,
         parsedPrice: isNaN(parsedPrice) ? 0 : parsedPrice,
@@ -52,13 +59,14 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
     });
 
     switch (selectedSort) {
-      case 'newest':
-        return parsedProducts.sort((a, b) =>
-          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      case "newest":
+        return parsedProducts.sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
         );
-      case 'lowest':
+      case "lowest":
         return parsedProducts.sort((a, b) => a.parsedPrice - b.parsedPrice);
-      case 'highest':
+      case "highest":
         return parsedProducts.sort((a, b) => b.parsedPrice - a.parsedPrice);
       default:
         return filteredProducts;
@@ -66,55 +74,73 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
   }, [filteredProducts, selectedSort]);
 
   const handleSeeAllPress = () => {
-    navigation.navigate('AllProductsScreen', { products });
+    navigation.navigate("AllProductsScreen", { products });
   };
 
-  const renderCategoryItem = ({ item }: { item:any }) => (
-    <TouchableOpacity style={styles.categoryItem} onPress={() => setSelectedCategory(item.name)}>
+  const renderCategoryItem = ({ item }: { item: any }) => (
+    <TouchableOpacity
+      style={[
+        styles.categoryItem,
+        selectedCategory === item.name && styles.selectedCategoryItem,
+      ]}
+      onPress={() => setSelectedCategory(item.name)}
+    >
       <Image source={{ uri: item.image }} style={styles.categoryImage} />
       <Text style={styles.categoryText}>{item.name}</Text>
     </TouchableOpacity>
   );
-  
+
   const renderProductItem = ({ item }: { item: Product }) => {
-     const isFavorite = favoriteProducts.some(product => product.id === item.id);
-    const isCart = cartProducts.some(product => product.id === item.id);
-    
+    const isFavorite = favoriteProducts.some(
+      (product) => product.id === item.id
+    );
+    const isCart = cartProducts.some((product) => product.id === item.id);
+
     return (
       <View style={styles.productItem}>
         <View style={styles.productImageContainer}>
           <Image source={{ uri: item.image }} style={styles.productImage} />
-           <TouchableOpacity
-          style={styles.favoriteIcon}
-          onPress={() => toggleFavorite(item)}
-        >
-             <Ionicons
-              name={isFavorite ? 'heart' : 'heart-outline'}
+          <TouchableOpacity
+            style={styles.favoriteIcon}
+            onPress={() => toggleFavorite(item)}
+          >
+            <Ionicons
+              name={isFavorite ? "heart" : "heart-outline"}
               size={24}
-              color={isFavorite ? Colors.TOGGLE_ICON_ERROR : Colors.PROFILE_OPTION_TOGGLE_DISABLE}
+              color={
+                isFavorite
+                  ? Colors.TOGGLE_ICON_ERROR
+                  : Colors.PROFILE_OPTION_TOGGLE_DISABLE
+              }
             />
-        </TouchableOpacity>
-           <TouchableOpacity
-          style={styles.cartIcon}
-          onPress={() => togglecart(item)}
-        >
-           <Ionicons
-              name={isCart ? 'cart' : 'cart-outline'}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.cartIcon}
+            onPress={() => togglecart(item)}
+          >
+            <Ionicons
+              name={isCart ? "cart" : "cart-outline"}
               size={24}
-              color={isCart ?  Colors.TOGGLE_ICON_ERROR : Colors.PROFILE_OPTION_TOGGLE_DISABLE}
+              color={
+                isCart
+                  ? Colors.TOGGLE_ICON_ERROR
+                  : Colors.PROFILE_OPTION_TOGGLE_DISABLE
+              }
             />
-        </TouchableOpacity>
+          </TouchableOpacity>
         </View>
         <Text style={styles.productName}>{item.name}</Text>
         <View style={styles.productPriceContainer}>
           <Text style={styles.productPrice}>${item.price}</Text>
-          {item.oldPrice && <Text style={styles.oldPrice}>${item.oldPrice}</Text>}
+          {item.oldPrice && (
+            <Text style={styles.oldPrice}>${item.oldPrice}</Text>
+          )}
         </View>
       </View>
     );
   };
 
-   const openModal = (type:any) => {
+  const openModal = (type: any) => {
     setModalType(type);
     setModalVisible(true);
   };
@@ -123,7 +149,7 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
     setModalVisible(false);
   };
 
-  const handleSortSelection = (option:any) => {
+  const handleSortSelection = (option: any) => {
     setSelectedSort(option);
     closeModal();
   };
@@ -132,7 +158,14 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
       return (
         <View style={styles.filterContainer}>
           <TouchableOpacity style={styles.filterOption} onPress={openModal}>
-            <Text style={[styles.filterText, selectedSort ? styles.selectedFilter : undefined]}>Sort by</Text>
+            <Text
+              style={[
+                styles.filterText,
+                selectedSort ? styles.selectedFilter : undefined,
+              ]}
+            >
+              Sort by
+            </Text>
           </TouchableOpacity>
         </View>
       );
@@ -144,7 +177,12 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.searchContainer}>
-          <Icon name="search-outline" size={20} color="#333" style={styles.searchIcon} />
+          <Icon
+            name="search-outline"
+            size={20}
+            color="#333"
+            style={styles.searchIcon}
+          />
           <TextInput
             style={styles.searchInput}
             placeholder="Search"
@@ -153,7 +191,7 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
           />
         </View>
 
-         {searchText === '' ? (
+        {searchText === "" ? (
           <>
             <View style={styles.section}>
               <View style={styles.sectionHeader}>
@@ -189,15 +227,25 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
             {renderFilterOptions()}
             {sortedProducts.length === 0 ? (
               <View style={styles.noResultsContainer}>
-                <Image source={require('@/assets/images/search.png')} style={styles.image} />
-                  <Text style={styles.message}>{TEXT.NO_SEARCH_PRODUCTS}</Text>
-                <TouchableOpacity style={styles.button} onPress={() => setSearchText('')}>
-                    <Text style={styles.buttonText}>{TEXT.EXPLORE_CATEGORIES}</Text>
+                <Image
+                  source={require("@/assets/images/search.png")}
+                  style={styles.image}
+                />
+                <Text style={styles.message}>{TEXT.NO_SEARCH_PRODUCTS}</Text>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => setSearchText("")}
+                >
+                  <Text style={styles.buttonText}>
+                    {TEXT.EXPLORE_CATEGORIES}
+                  </Text>
                 </TouchableOpacity>
               </View>
             ) : (
               <View style={styles.section}>
-                <Text style={styles.resultsCount}>{sortedProducts.length} Results Found</Text>
+                <Text style={styles.resultsCount}>
+                  {sortedProducts.length} Results Found
+                </Text>
                 <FlatList
                   data={sortedProducts}
                   renderItem={renderProductItem}
@@ -220,17 +268,47 @@ const { favoriteProducts, toggleFavorite } = useFavorites();
       >
         <View style={styles.modalBackground}>
           <View style={styles.modalContent}>
-            <TouchableOpacity onPress={() => handleSortSelection('recommended')}>
-              <Text style={[styles.sortOptionText, selectedSort === 'recommended' && styles.selectedSortOption]}>Recommended</Text>
+            <TouchableOpacity
+              onPress={() => handleSortSelection("recommended")}
+            >
+              <Text
+                style={[
+                  styles.sortOptionText,
+                  selectedSort === "recommended" && styles.selectedSortOption,
+                ]}
+              >
+                Recommended
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleSortSelection('newest')}>
-              <Text style={[styles.sortOptionText, selectedSort === 'newest' && styles.selectedSortOption]}>Newest</Text>
+            <TouchableOpacity onPress={() => handleSortSelection("newest")}>
+              <Text
+                style={[
+                  styles.sortOptionText,
+                  selectedSort === "newest" && styles.selectedSortOption,
+                ]}
+              >
+                Newest
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleSortSelection('lowest')}>
-              <Text style={[styles.sortOptionText, selectedSort === 'lowest' && styles.selectedSortOption]}>Lowest - Highest Price</Text>
+            <TouchableOpacity onPress={() => handleSortSelection("lowest")}>
+              <Text
+                style={[
+                  styles.sortOptionText,
+                  selectedSort === "lowest" && styles.selectedSortOption,
+                ]}
+              >
+                Lowest - Highest Price
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleSortSelection('highest')}>
-              <Text style={[styles.sortOptionText, selectedSort === 'highest' && styles.selectedSortOption]}>Highest - Lowest Price</Text>
+            <TouchableOpacity onPress={() => handleSortSelection("highest")}>
+              <Text
+                style={[
+                  styles.sortOptionText,
+                  selectedSort === "highest" && styles.selectedSortOption,
+                ]}
+              >
+                Highest - Lowest Price
+              </Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
               <Text style={styles.buttonText}>Close</Text>
@@ -252,8 +330,8 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
   },
   searchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingHorizontal: 20,
     marginBottom: 20,
     backgroundColor: Colors.BACKBUTTONBACKGROUND,
@@ -272,14 +350,14 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   sectionHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 10,
   },
   sectionTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   seeAll: {
     fontSize: 14,
@@ -287,7 +365,7 @@ const styles = StyleSheet.create({
   },
   categoryItem: {
     marginRight: 15,
-    alignItems: 'center',
+    alignItems: "center",
   },
   categoryImage: {
     width: 60,
@@ -297,34 +375,34 @@ const styles = StyleSheet.create({
   },
   categoryText: {
     fontSize: 14,
-    textAlign: 'center',
+    textAlign: "center",
   },
   productList: {
     paddingBottom: 16,
   },
   columnWrapper: {
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
-  
-   productItem: {
+
+  productItem: {
     flex: 1,
     backgroundColor: Colors.WHITE100,
     borderRadius: 8,
     padding: 8,
     margin: 8,
-    alignItems: 'center',
+    alignItems: "center",
   },
   productImageContainer: {
-    position: 'relative',
+    position: "relative",
     marginBottom: 10,
   },
   productImage: {
     width: 150,
     height: 150,
-    marginBottom:8,
+    marginBottom: 8,
   },
   favoriteIcon: {
-    position: 'absolute',
+    position: "absolute",
     top: 10,
     right: 10,
     backgroundColor: Colors.BLACK,
@@ -333,7 +411,7 @@ const styles = StyleSheet.create({
   },
   productName: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   productPrice: {
     fontSize: 14,
@@ -341,19 +419,19 @@ const styles = StyleSheet.create({
   },
   oldPrice: {
     fontSize: 12,
-    color: '#999',
-    textDecorationLine: 'line-through',
+    color: "#999",
+    textDecorationLine: "line-through",
   },
   filterContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     paddingHorizontal: 20,
     marginBottom: 20,
   },
   filterOption: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     paddingVertical: 10,
   },
   filterText: {
@@ -361,13 +439,13 @@ const styles = StyleSheet.create({
     color: Colors.PRODUCT_PRICE,
   },
   selectedFilter: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: Colors.NOTIFICATION_BUTTON,
   },
   noResultsContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingHorizontal: 20,
   },
   image: {
@@ -378,7 +456,7 @@ const styles = StyleSheet.create({
   message: {
     fontSize: 16,
     color: Colors.PRODUCT_PRICE,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 20,
   },
   button: {
@@ -393,29 +471,29 @@ const styles = StyleSheet.create({
   },
   resultsCount: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingHorizontal: 20,
     marginBottom: 10,
   },
   modalBackground: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: Colors.BLACK,
   },
   modalContent: {
-    width: '80%',
+    width: "80%",
     backgroundColor: Colors.BACKBUTTONBACKGROUND,
     borderRadius: 10,
     padding: 20,
-    alignItems: 'center',
+    alignItems: "center",
   },
   sortOptionText: {
     fontSize: 16,
     paddingVertical: 10,
   },
   selectedSortOption: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: Colors.NOTIFICATION_BUTTON,
   },
   closeButton: {
@@ -423,22 +501,24 @@ const styles = StyleSheet.create({
   },
 
   productPriceContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   cartButton: {
     marginLeft: 10,
   },
 
   cartIcon: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 8,
     right: 8,
     backgroundColor: Colors.BACKBUTTONBACKGROUND,
     borderRadius: 16,
     padding: 4,
   },
+  selectedCategoryItem: {
+    borderBottomWidth: 2,
+    borderBottomColor: Colors.BUTTON,
+  },
 });
-
-
